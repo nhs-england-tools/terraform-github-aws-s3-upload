5b7c77f77a6ac5acbbfde47f1a6b1239d6c170ee
Amaan Ibn-Nasar <amaan.ibn-nasar1@nhs.net>
2023-05-05 13:29:12 +0100

    fixed the example terraform

---
+++ examples/react-app/locals.tf
@@ -0,0 +1,4 @@
+locals {
+s3_bucket_name = "terraform-github-action-s3-upload"
+
+}
--- examples/react-app/main.tf
+++ examples/react-app/main.tf
@@ -1,11 +1,12 @@
 module "github_action_s3_upload" {
-  source            = "github.com/nhs-england-tools/terraform-github-action-s3-upload?ref=v0.0.1-dev"
-  project_name      = "react-app-s3-upload"
-  s3_bucket_name    = "react-app-frontend"
+  source            = "github.com/nhs-england-tools/terraform-github-action-s3-upload?ref=v0.0.5-dev"
+  project_name      = "terraform-github-action-s3-upload"
+  s3_bucket_name    = local.s3_bucket_name
   s3_bucket_actions = ["s3:PutObject"]
-  s3_bucket_resources = ["arn:aws:s3:::react-app-frontend/index.html",
-    "arn:aws:s3:::react-app-frontend/assets/*.js",
-  "arn:aws:s3:::react-app-frontend/*.css"]
-  github_organisation = "NHS-Organisation"
-  github_repo         = "react-app-frontend"
+  s3_bucket_resources = ["arn:aws:s3:::${local.s3_bucket_name}/index.html",
+    "arn:aws:s3:::${local.s3_bucket_name}/assets/*.js",
+  "arn:aws:s3:::${local.s3_bucket_name}/*.css"]
+  github_organisation = "nhs-england-tools"
+  github_repo         = "terraform-github-action-s3-upload"
+  github_branch = "ENG-XXX-create_github_action_module"
 }
