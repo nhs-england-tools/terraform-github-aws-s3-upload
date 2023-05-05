1007d6c1321737e16435aa0d37585b2133ff6dc5
Amaan Ibn-Nasar <amaan.ibn-nasar1@nhs.net>
2023-05-05 13:50:05 +0100

    Gave action access to more resources

--- examples/react-app/main.tf
+++ examples/react-app/main.tf
@@ -5,7 +5,10 @@ module "github_action_s3_upload" {
   s3_bucket_actions = ["s3:PutObject"]
   s3_bucket_resources = ["arn:aws:s3:::${local.s3_bucket_name}/index.html",
     "arn:aws:s3:::${local.s3_bucket_name}/assets/*.js",
-  "arn:aws:s3:::${local.s3_bucket_name}/*.css"]
+    "arn:aws:s3:::${local.s3_bucket_name}/assets/*.css",
+    "arn:aws:s3:::${local.s3_bucket_name}/assets/*.svg",
+    "arn:aws:s3:::${local.s3_bucket_name}/*.svg",
+  ]
   github_organisation = "nhs-england-tools"
   github_repo         = "terraform-github-action-s3-upload"
   github_branch = "ENG-XXX-create_github_action_module"
